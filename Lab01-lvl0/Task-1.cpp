552fe890e6987e4f0f12e1ead001ca4bb5ecf8ae
Vyacheslav <slava.shitov.00@mail.ru>
2018-12-20 10:42:22 +0500

    Remake
    up

--- Lab01-lvl0/Task-1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-using namespace std;
-
-int main()
-{
-	cout << "Hello, stranger.\n\nThis is your first program, call me Laboratory 1 \ Task 1.\n\nYours, C++ " << endl;
-	;		return 0;
-}
